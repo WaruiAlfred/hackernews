680755d30910bd8b3521992333f3def608428fad
WaruiAlfred <alfredwarui17@gmail.com>
2022-12-19 23:16:19 +0300

    set authLink to apollo client

--- src/index.js
+++ src/index.js
@@ -10,13 +10,25 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
+import { AUTH_TOKEN } from "./constants";
+import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
